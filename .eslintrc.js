c5477812ee96b99a31134f054871b8d1c43b5404
renanbastosc <renanbastos@live.com>
2022-03-12 01:59:29 -0300

    chore(): add configs

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  pluggins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-rassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorepattern: 'next' }],
+  },
+};
